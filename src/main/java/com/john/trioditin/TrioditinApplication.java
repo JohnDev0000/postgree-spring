235c531e0550acb0700d708b733398e0554c8036
JohnDev0000 <johncroft2236@hotmail.com>
2024-02-24 00:58:07 -0300

    first commit

---
+++ src/main/java/com/john/trioditin/TrioditinApplication.java
@@ -0,0 +1,13 @@
+package com.john.trioditin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrioditinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrioditinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/john/trioditin/TrioditinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.trioditin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrioditinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
